c2f9bb9acd9e18ac119b18ca345df779c46cc39a
Dziugas Juozapaitis <dziugasj@gmail.com>
2023-02-04 14:48:43 +0200

    Removed clientOrders; Added demo nutrients

---
+++ src/main/resources/db/migration/V4_0__insert_demo_nutritional_values.sql
@@ -0,0 +1,41 @@
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MACRO' , 'FAT', '', 'G', 2.5);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MACRO' , 'CARB', '', 'G', 55);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MACRO' , 'FIBRE', '', 'G', 0.5);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MACRO' , 'PROTEIN', '', 'G', 8.5);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MICRO' , 'VIT_D', '', 'MCG', 1.2);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MICRO' , 'POTASSIUM', '', 'MG', 190);
+
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (2, 'MACRO' , 'FAT', '', 'G', 3.3);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (2, 'MACRO' , 'CARB', '', 'G', 30);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (2, 'MACRO' , 'FIBRE', '', 'G', 0.9);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (2, 'MACRO' , 'PROTEIN', '', 'G', 17.5);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (2, 'MICRO' , 'VIT_D', '', 'MCG', 0.2);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (2, 'MICRO' , 'POTASSIUM', '', 'MG', 120);
+
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (3, 'MACRO' , 'FAT', '', 'G', 8.3);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (3, 'MACRO' , 'CARB', '', 'G', 33.4);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (3, 'MACRO' , 'FIBRE', '', 'G', 1.9);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (3, 'MACRO' , 'PROTEIN', '', 'G', 22.9);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (3, 'MICRO' , 'VIT_D', '', 'MCG', 1.2);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (3, 'MICRO' , 'POTASSIUM', '', 'MG', 190);
+
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MACRO' , 'FAT', '', 'G', 2.5);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MACRO' , 'CARB', '', 'G', 55);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MACRO' , 'FIBRE', '', 'G', 0.5);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MACRO' , 'PROTEIN', '', 'G', 8.5);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MICRO' , 'CALCIUM', '', 'MG', 20);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MICRO' , 'POTASSIUM', '', 'MG', 190);
+
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MACRO' , 'FAT', '', 'G', 2.5);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MACRO' , 'CARB', '', 'G', 55);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MACRO' , 'FIBRE', '', 'G', 0.5);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MACRO' , 'PROTEIN', '', 'G', 8.5);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MICRO' , 'VIT_A', '', 'MCG', 80);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MICRO' , 'POTASSIUM', '', 'MG', 170);
+
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MACRO' , 'FAT', '', 'G', 44);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MACRO' , 'CARB', '', 'G', 34);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MACRO' , 'FIBRE', '', 'G', 0.7);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MACRO' , 'PROTEIN', '', 'G', 12.5);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MICRO' , 'VIT_D', '', 'MCG', 1.6);
+INSERT INTO nutritional_value (product_id, nutrient_type, macro_nutrient, micro_nutrient, nutrient_unit,  nutrient_value) VALUES (1, 'MICRO' , 'POTASSIUM', '', 'MG', 130);

ff82e993283a6a27837369b4e10e3fa1e60fe62c
Dziugas Juozapaitis <dziugasj@gmail.com>
2023-02-04 18:20:08 +0200

    Print stack trace on error

--- src/main/java/com/home/smoothieshop/rest/RestExceptionHandler.java
+++ src/main/java/com/home/smoothieshop/rest/RestExceptionHandler.java
@@ -2,26 +2,29 @@ package com.home.smoothieshop.rest;
 
 import com.home.smoothieshop.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
-public class RestExceptionHandler extends ResponseEntityExceptionHandler {
-
-    @ResponseBody
-    @ExceptionHandler(NotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNotFoundException() {
-        return "";
+public class RestExceptionHandler {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> handleConstraintViolationException(Exception exception) {
+        exception.printStackTrace();
+        return new ResponseEntity<>("", HttpStatus.BAD_REQUEST);
     }
 
-    @ResponseBody
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(Exception exception) {
+        exception.printStackTrace();
+        return new ResponseEntity<>("", HttpStatus.NOT_FOUND);
+    }
+
     @ExceptionHandler(RuntimeException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleRuntimeException() {
-        return "";
+    public ResponseEntity<String> handleRuntimeException(Exception exception) {
+        exception.printStackTrace();
+        return new ResponseEntity<>("", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }

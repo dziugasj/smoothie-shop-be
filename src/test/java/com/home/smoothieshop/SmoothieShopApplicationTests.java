bac04bf43f4e1196d1f009ea6018e94b9a7cd7a5
Dziugas Juozapaitis <dziugasj@gmail.com>
2023-02-05 12:10:00 +0200

    Integration test almost done

--- src/test/java/com/home/smoothieshop/SmoothieShopApplicationTests.java
+++ src/test/java/com/home/smoothieshop/SmoothieShopApplicationTests.java
@@ -1,9 +1,10 @@
 package com.home.smoothieshop;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.home.smoothieshop.dto.NutritionalValueDto;
 import com.home.smoothieshop.dto.ProductDto;
-import com.home.smoothieshop.dto.enums.ProductTypeDto;
+import com.home.smoothieshop.dto.enums.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,8 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Collections;
+import java.util.List;
 
+import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -36,14 +39,55 @@ class SmoothieShopApplicationTests {
     }
 
     @Test
-    void giveNewProduct_whenPostingNewProduct_thenStatusCreatedReturned() throws Exception {
-        ProductDto productDto = createProductStub();
+    void givenNewProduct_whenPostingNewProduct_thenStatusCreatedReturned() throws Exception {
+        ProductDto productDto = createProductDtoStub(createNutritionalValuesDtoStub());
 
         mockMvc.perform(post("/api/v1/products")
                         .content(objectMapper.writeValueAsString(productDto))
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    void givenNewProduct_whenPostingNewProductAndGettingSame_thenPostedDtoEqualsReturnedDto() throws Exception {
+        ProductDto productDto = createProductDtoStub(createNutritionalValuesDtoStub());
+        long productId = postNewProduct(productDto);
+
+        String payload = mockMvc.perform(get("/api/v1/products/{id}", productId))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        ProductDto returnedProduct = objectMapper.readValue(payload, ProductDto.class);
+
+        assertThat(returnedProduct)
+                .usingRecursiveComparison()
+                .ignoringFields("id", "nutritionalValues.id")
+                .isEqualTo(productDto);
+    }
+
+    @Test
+    void given2newProducts_whenPostingThemAndGettingProductList_thenThose2CreateMustBeInTheList() throws Exception {
+        ProductDto firstProductDto = createProductDtoStub(emptyList());
+        ProductDto secondProductDto = createProductDtoStub(emptyList());
+        long firstProductId = postNewProduct(firstProductDto);
+        long secondProductId = postNewProduct(secondProductDto);
+        var expectedProductList = List.of(
+                copyWithId(firstProductId, firstProductDto),
+                copyWithId(secondProductId, secondProductDto)
+        );
+
+        String payload = mockMvc.perform(get("/api/v1/products"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        List<ProductDto> returnedProductList = objectMapper.readValue(payload, new TypeReference<List<ProductDto>>() {
+        });
+
+        assertThat(returnedProductList)
+                .containsAll(expectedProductList);
     }
 
     private long postNewProduct(ProductDto productDto) throws Exception {
@@ -58,14 +102,52 @@ class SmoothieShopApplicationTests {
         return Long.parseLong(createdProductIdResponse);
     }
 
-    private ProductDto createProductStub() {
+    private ProductDto createProductDtoStub(List<NutritionalValueDto> nutritionalValues) {
         return new ProductDto(
                 null,
                 ProductTypeDto.SMOOTHIE,
                 "Weekend fun",
                 "Drink for those who are having fun time on the weekend",
-                Collections.emptyList()
+                nutritionalValues
         );
     }
 
+    private List<NutritionalValueDto> createNutritionalValuesDtoStub() {
+        return List.of(
+                createMacroNutritionalValueDtoStub(),
+                createMicroNutritionalValueDtoStub()
+        );
+    }
+
+    private NutritionalValueDto createMacroNutritionalValueDtoStub() {
+        return new NutritionalValueDto(
+                null,
+                NutrientTypeDto.MACRO,
+                MacroNutrientDto.FAT,
+                null,
+                NutrientUnitDto.G,
+                5.4
+        );
+    }
+
+    private NutritionalValueDto createMicroNutritionalValueDtoStub() {
+        return new NutritionalValueDto(
+                null,
+                NutrientTypeDto.MICRO,
+                null,
+                MicroNutrientDto.VIT_A,
+                NutrientUnitDto.MG,
+                180.0
+        );
+    }
+
+    private ProductDto copyWithId(long id, ProductDto productDto) {
+        return new ProductDto(
+                id,
+                productDto.productType(),
+                productDto.name(),
+                productDto.basicDetails(),
+                productDto.nutritionalValues()
+        );
+    }
 }
